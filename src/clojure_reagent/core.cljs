09ca61b8c63296c87174477493f19f841ee18f10
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2017-12-15 17:16:27 +0200

    Add cjols

--- src/clojure_reagent/core.cljs
+++ src/clojure_reagent/core.cljs
@@ -4,15 +4,14 @@
 
 (def log (.-log js/console))
 
-(log (/ 5 2))
 
 (def click-count (r/atom 0))
-(defn on_click_count_change [key atom old new] (log "old:" old "new:" new ))
+(defn on_click_count_change [key atom old new] (if (< new 3) (log "old:" old "new:" new ) (log "more than 3:" new)))
 (add-watch click-count 'what?' on_click_count_change)
 
 (def model1 (r/atom {:some 423 :test 55}))
 
-(defn click_count_change [num] [:span  {:style {:color "blue" :backgroundColor "gray" }} "num:" num] )
+(defn click_count_change [num] [:span  {:style {:color "blue" :backgroundColor "gray" }} num] )
 
 ;; -------------------------
 ;; Views
@@ -35,8 +34,10 @@
 	    [:button  {:on-click #(swap! click-count dec)} "Ddecrement"]
 	    [:button  {:on-click #(compare-and-set! click-count 2 42)} "rom 2 to 42!!"]
 	    [:hr]
-	    [:span (click_count_change 42)]
-	    [test/test_fn 42]
+	    [:span "click_count_change: " (click_count_change 42)]
+	    [:hr]
+	    [:div (if (= @click-count 5) "5 Clicks done..." "Make 5 clicks plz..")]
+	    (test/test_fn (* @click-count 2))
 	])
 
 
